1c1e59e48eabe1f60b8690de52eabcee266b0496
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-08 17:25:29 -0600

    setup items gql module and postgres docker compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
+import { join } from "path";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { GraphQLModule } from "@nestjs/graphql";
 import { Module } from "@nestjs/common";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { ItemsModule } from "./items/items.module";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      playground: false,
+      includeStacktraceInErrorResponses: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ]
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
