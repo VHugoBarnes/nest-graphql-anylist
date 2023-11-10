889e1897aea0ee58390672f5e1ebe68ada46a659
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-09 20:14:40 -0600

    protecting who can query users (only admins and superUsers)

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,21 +2,32 @@ import { Resolver, Query, Mutation, Args, ID } from "@nestjs/graphql";
 import { UsersService } from "./users.service";
 import { User } from "./entities/user.entity";
 import { ValidRolesArgs } from "./dto";
+import { ParseUUIDPipe, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
+import { CurrentUser } from "src/auth/decorators/current-user.decorator";
+import { ValidRoles } from "src/auth/enums/valid-roles.enum";
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) { }
 
   @Query(() => [User], { name: "users" })
   findAll(
-    @Args() validRoles: ValidRolesArgs
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User
   ): Promise<User[]> {
+    console.log({ user });
     return this.usersService.findAll(validRoles.roles);
   }
 
   @Query(() => User, { name: "user" })
-  findOne(@Args("id", { type: () => ID }) id: string): Promise<User> {
-    return this.usersService.findOne(id);
+  findOne(
+    @Args("id", { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User
+  ): Promise<User> {
+    console.log({ user });
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
