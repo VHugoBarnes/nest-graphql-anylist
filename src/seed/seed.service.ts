2c1e8db2e6308f5f38abf7c46c4ff7a90f367d8c
Victor Hugo Vazquez Gomez <hugovg799@gmail.com>
2023-11-10 17:57:36 -0600

    delete item and user table content

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,20 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Item } from "src/items/entities/item.entity";
+import { User } from "src/users/entities/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
   constructor(
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>
   ) {
     this.isProd = configService.get("state") === "prod";
   }
@@ -15,10 +23,25 @@ export class SeedService {
     if (this.isProd) throw new UnauthorizedException("[cant-run-seed]");
 
     // Clean database
+    await this.deleteDatabase();
 
     // Load users
 
     // Load items
     return true;
+  }
+
+  async deleteDatabase() {
+    // delete items
+    await this.itemRepository.createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+
+    // delete users
+    await this.userRepository.createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
